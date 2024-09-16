601486cbb6046ed22ab838c92886e19f251f2c22
ayabarakat3 <ayabarakat14@gmail.com>
2024-09-16 17:37:47 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,10 @@
+// Task 1: Create an Inventory Array of Product Objects
+// An array will be created to label items chosen. 
+let Inventory= [
+    { name: 'Spanish Latte', price: $6, quantity: 90 },
+    { name: 'Pistachio Latte', price: $7, quantity: 76 },
+    { name: 'Americano', price: $4, quantity: 104 },
+    { name: 'Caramel Latte', price: $6, quantity: 85 },
+    { name: 'Turkish Coffee', price: $5, quantity: 68}];
+// Array of five items has been created. 
+// Task 1 is now completed.
